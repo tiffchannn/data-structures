f7c7141a3dc0ae1cc1173bc3d0f8e2568890ca10
tiffchannn <tiffany.yi.chan@gmail.com>
2020-08-21 10:38:15 -0700

    Successfully passed tests for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  //use extend to pull in treeMethods
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +14,41 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // takes any value, sets that as the target of a node, and adds that node as a child of the tree
+  // set tree = value
+  this.children.push(Tree(value));
+
 };
 
 treeMethods.contains = function(target) {
+  // takes any input and returns a boolean reflecting whether it can be found as the value of the target node/newTree or any descendant/child node
+
+  // create isFound flag to boolean false
+  var isFound = false;
+  // create function to check if value is found within newTree
+  var search = function(branch) {
+    // if found, return true
+    if (branch.value === target) {
+      isFound = true;
+      return isFound;
+    }
+    //loop through children
+    for (var i = 0 ; i < branch.children.length; i++) {
+      // use recursion on each child
+      search(branch.children[i]);
+    }
+  }
+  //call the search function on newTree (this)
+  search(this);
+  // return isFound
+  return isFound;
+
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+    treeMethods.addChild = constant time complexity
+    treeMethods.contains = linear time complexity
  */
