4521f0f0a3854b56884d2cdec4c59cd19d0f348b
tiffchannn <tiffany.yi.chan@gmail.com>
2020-08-21 19:56:44 -0700

    Successfully passed tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage[item] = 'str';
 };
 
 setPrototype.contains = function(item) {
+  if (this.storage[item]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+
 setPrototype.remove = function(item) {
+
+  if (this.storage[item]) {
+    delete this.storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  all three functions are constant time complexity
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,10 +2,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = [];  // fix me
+  newTree.children = [];
 
-  //use extend to pull in treeMethods
   _.extend(newTree, treeMethods);
 
   return newTree;
@@ -14,33 +12,28 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // takes any value, sets that as the target of a node, and adds that node as a child of the tree
-  // set tree = value
+
   this.children.push(Tree(value));
 
 };
 
 treeMethods.contains = function(target) {
-  // takes any input and returns a boolean reflecting whether it can be found as the value of the target node/newTree or any descendant/child node
 
-  // create isFound flag to boolean false
   var isFound = false;
-  // create function to check if value is found within newTree
+
   var search = function(branch) {
-    // if found, return true
+
     if (branch.value === target) {
       isFound = true;
       return isFound;
     }
-    //loop through children
-    for (var i = 0 ; i < branch.children.length; i++) {
-      // use recursion on each child
+
+    for (var i = 0; i < branch.children.length; i++) {
       search(branch.children[i]);
     }
-  }
-  //call the search function on newTree (this)
+  };
+
   search(this);
-  // return isFound
   return isFound;
 
 };
